535789c99175a2ffbb5fed480fdae42b9088e6bb
Kim Dam Grønhøj <kidg@techcollege.dk>
2017-09-18 12:12:37 +0200

    Add project files.

---
+++ bookingMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bookingMVC.Startup))]
+namespace bookingMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
